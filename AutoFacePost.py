12a4c767795d725a8abcd168962a5af19b3e07c2
italo192 <117384110+italo192@users.noreply.github.com>
2023-11-01 12:12:05 -0300

    Update AutoFacePost.py

--- AutoFacePost.py
+++ AutoFacePost.py
@@ -50,7 +50,7 @@ para_escrever = driver.find_element(By.XPATH,'//div[@class="x1i10hfl x6umtig x1b
 para_escrever.click()
 sleep(10)
 digitar = driver.find_element(By.XPATH,'//p[@class="xdj266r x11i5rnm xat24cr x1mh8g0r x16tdsg8"]')
-digitar.send_keys('boa tarde')
+digitar.send_keys('mensagem')
 sleep(2)
 publicar = driver.find_element(By.XPATH,'//div[@class="x1n2onr6 x1ja2u2z x78zum5 x2lah0s xl56j7k x6s0dn4 xozqiw3 x1q0g3np xi112ho x17zwfj4 x585lrc x1403ito x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xn6708d x1ye3gou xtvsq51 x1r1pt67"]')
 publicar.click()
